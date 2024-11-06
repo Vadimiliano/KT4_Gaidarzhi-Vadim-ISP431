158f30a723753460e97edbc906c698849d88b856
comcuru <comcuru@DJ-PC-K207-02>
2024-11-06 12:01:58 +0300

    FirstVersion

---
+++ SportShopApp/SportShopApp/Pages/ListPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportShopApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListPage.xaml
+    /// </summary>
+    public partial class ListPage : Page
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+            UserListview.ItemsSource = Data.EmployeeBDEntities.GetContext().Users.ToList();
+        }
+
+        private void EditUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
